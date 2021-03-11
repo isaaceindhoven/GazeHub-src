c26cf4230c4e35e5b460e64d73fc680c0a5fd755
Bader Ali <bader.ali@isaac.nl>
2021-03-11 14:47:54 +0100

    Add copyright preamble to source files

--- src/Router.php
+++ src/Router.php
@@ -1,5 +1,14 @@
 <?php
 
+/**
+  *   Do not remove or alter the notices in this preamble.
+  *   This software code regards ISAAC Standard Software.
+  *   Copyright © 2021 ISAAC and/or its affiliates.
+  *   www.isaac.nl All rights reserved. License grant and user rights and obligations
+  *   according to applicable license agreement. Please contact sales@isaac.nl for
+  *   questions regarding license and user rights.
+  */
+
 declare(strict_types=1);
 
 namespace GazeHub;
@@ -29,27 +38,26 @@ class Router
     public function route(ServerRequestInterface $request): Response
     {
         $path = $request->getUri()->getPath();
+        $method = $request->getMethod();
 
         $routes = require(__DIR__ . '/../config/routes.php');
 
-        $method = $request->getMethod();
-
         $endPointExist = array_key_exists($method, $routes) && (array_key_exists($path, $routes[$method]));
+
+        if (!$endPointExist) {
+            return new Response(404, [], 'Not found');
+        }
 
         $req = $this->container->get(Request::class);
         $req->setOriginalRequest($request);
 
-        if ($endPointExist) {
-            $endPoint = $routes[$method][$path];
-            $handler = [ $this->container->get($endPoint[0]), $endPoint[1] ];
+        $endPoint = $routes[$method][$path];
+        $handler = [ $this->container->get($endPoint[0]), $endPoint[1] ];
 
-            try {
-                return call_user_func($handler, $req);
-            } catch (DataValidationFailedException $e) {
-                return new Response(400, ['Content-Type' => 'application/json'], json_encode($e->errors));
-            }
+        try {
+            return call_user_func($handler, $req);
+        } catch (DataValidationFailedException $e) {
+            return new Response(400, ['Content-Type' => 'application/json'], json_encode($e->errors));
         }
-
-        return new Response(404, [], 'Not found');
     }
 }
