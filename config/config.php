03ae14414df2218ffa5fac099c651e0af32a2906
Bader Ali <bader.ali@isaac.nl>
2021-04-29 13:17:55 +0200

    Disable token verify if no public key present in config

--- config/config.php
+++ config/config.php
@@ -16,5 +16,5 @@ return [
     'host' => '0.0.0.0',
     'jwt_public_key' => '',
     'jwt_alg' => 'RS256',
-    'log_level' => 'INFO', // ['DEBUG', 'INFO', 'ERROR']
+    'log_level' => 'INFO', // ['DEBUG', 'INFO', 'WARN', 'ERROR']
 ];
--- src/Providers/TokenDecoderProvider.php
+++ src/Providers/TokenDecoderProvider.php
@@ -16,11 +16,25 @@ namespace ISAAC\GazeHub\Providers;
 use DI\Container;
 use ISAAC\GazeHub\Decoders\TokenDecoder;
 use ISAAC\GazeHub\Decoders\TokenDecoderJwt;
+use ISAAC\GazeHub\Decoders\TokenDecoderJwtPlacebo;
+use ISAAC\GazeHub\Repositories\ConfigRepository;
+use Psr\Log\LoggerInterface;
 
 class TokenDecoderProvider implements Provider
 {
     public function register(Container &$container): void
     {
-        $container->set(TokenDecoder::class, $container->get(TokenDecoderJwt::class));
+        $configRepo = $container->get(ConfigRepository::class);
+
+        $publicKey = $configRepo->get('jwt_public_key');
+
+        if ($publicKey === '') {
+            /** @var LoggerInterface */
+            $logger = $container->get(LoggerInterface::class);
+            $logger->warning('No public key provided. Token decode will not validate JWT. DONT USE IN PRODUCTION');
+            $container->set(TokenDecoder::class, $container->get(TokenDecoderJwtPlacebo::class));
+        } else {
+            $container->set(TokenDecoder::class, $container->get(TokenDecoderJwt::class));
+        }
     }
 }
